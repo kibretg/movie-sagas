1bba0624ab0c19560d6ed2c2eaecb765e9dcf254
kibretg <kibretrg@gmail.com>
2019-06-22 14:36:15 -0500

    allows for information to append to dom from server

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -1,10 +1,27 @@
 const express = require('express');
+
+const pool = require('../modules/pool');
 const router = express.Router();
-const movieList = [];
 
 router.get('/', (req, res) => {
-    res.send(movieList);
+    const queryText = 'SELECT * FROM "movies"';
+    pool.query(queryText)
+    .then((result) => {res.send(result.rows); })
+    .catch((error) => {
+        console.log('Error completeing SELECT query', error);
+        res.sendStatus(500); 
+    });
 });
+
+router.get('/details/:id', (req, res) => {
+    const queryText = 'SELECT * FROM "movies" WHERE id=$1';
+    pool.query(queryText, [req.params.id])
+      .then((result) => { res.send(result.rows); })
+      .catch((err) => {
+        console.log('Error completing SELECT plant query', err);
+        res.sendStatus(500);
+      });
+  });
 
 router.post('/',(req, res) => {
     res.sendStatus(201);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import './App.css';
 
 class App extends Component {
   // Renders the entire app on the DOM
+  getMovies = () => {
+    this.props.dispatch({type: 'FETCH_MOVIES'})
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <pre>
+        {JSON.stringify(this.props.reduxState)}
+        </pre>
       </div>
     );
   }
 }
 
-export default App;
+const mapReduxStateToProps = reduxState => ({
+  reduxState
+});
+
+
+
+export default connect(mapReduxStateToProps) (App);
